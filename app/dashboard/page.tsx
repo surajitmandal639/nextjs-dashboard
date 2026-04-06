2eb80de8c492de64ab972645347628b7c58da6d9
surajit <surajit.mandal639@gmail.com>
2026-04-06 20:20:34 +0530

    database

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,35 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+export default async function Dashboard() {
+  const res = await fetch('http://localhost:3000/query', {
+    cache: 'no-store',
+  })
+
+  const data = await res.json()
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>📊 Invoices Dashboard</h1>
+
+      <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr>
+            <th style={{ border: '1px solid black' }}>Name</th>
+            <th style={{ border: '1px solid black' }}>Amount</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((item: any, index: number) => (
+            <tr key={index}>
+              <td style={{ border: '1px solid black', padding: '8px' }}>
+                {item.name}
+              </td>
+              <td style={{ border: '1px solid black', padding: '8px' }}>
+                ${item.amount}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
--- app/query/route.ts
+++ app/query/route.ts
@@ -6,18 +6,17 @@ async function listInvoices() {
 	const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
+    JOIN customers ON invoices.customer_id = customers.id;
   `;
 
 	return data;
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  // return Response.json({
+  //   message:
+  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  // });
   try {
   	return Response.json(await listInvoices());
   } catch (error) {
