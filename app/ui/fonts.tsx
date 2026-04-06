89d506e6fc175f8935f2469b0c1859878af59ed8
surajit <surajit.mandal639@gmail.com>
2026-04-06 18:40:52 +0530

    initial commit

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,20 @@
+// import { Inter, Roboto } from 'next/font/google';
+
+// // Inter font
+// export const inter = Inter({
+//   subsets: ['latin'],
+//   variable: '--font-inter',
+// });
+
+// // Roboto font (optional)
+// export const roboto = Roboto({
+//   subsets: ['latin'],
+//   weight: ['400', '700'],
+//   variable: '--font-roboto',
+// });
+
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700']});
